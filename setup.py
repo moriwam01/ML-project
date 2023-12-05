fb18416abb94fb3cb5b7f3759d7f91e605429f0e
Moriwam <mariwammimi99@gmail.com>
2023-12-05 23:41:13 +0600

    added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ML-project",
+    version="0.0.1",
+    author="Moriwam",
+    author_email="mariwammimi99@gmail.com",
+    packages=find_packages(),
+    install_requires=["pandas", "numpy", "scikit-learn", "seaborn"],
+)
